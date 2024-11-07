e4493276fa6097199cbf45c50537c36bd3b1385f
chanhihi <chanhihi55@gmail.com>
2024-11-07 18:27:24 +0900

    build: TCA 프레임워크 적용

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -1,22 +1,17 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 import PackageDescription
 
 #if TUIST
-    import ProjectDescription
+    import struct ProjectDescription.PackageSettings
 
     let packageSettings = PackageSettings(
-        // Customize the product types for specific package product
-        // Default is .staticFramework
-        // productTypes: ["Alamofire": .framework,] 
-        productTypes: [:]
+        productTypes: ["ComposableArchitecture": .framework]
     )
 #endif
 
 let package = Package(
     name: "UsicRoom",
     dependencies: [
-        // Add your own dependencies here:
-        // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
-        // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.15.2"),
     ]
 )
